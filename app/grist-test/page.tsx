a9f331122dec7304296aaee985c56217422c1d3a
kaptainkangaroo <karlenchang@gmail.com>
2025-01-25 15:14:09 +0800

    spike - added record display page

---
+++ app/grist-test/page.tsx
@@ -0,0 +1,10 @@
+export default function GristTestPage() {
+    return `
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta http-equiv="refresh" content="0;url=/grist-test.html" />
+            </head>
+        </html>
+    `;
+}
